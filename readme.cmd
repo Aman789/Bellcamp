d9f60fc19de0fec8e1ee5462cd6eef5fc2b3d558
root <asingh78980@gmail.com>
2020-02-16 19:25:50 +0000

    updating files

--- readme.cmd
+++ readme.cmd
@@ -4,3 +4,15 @@ touch app.js
 
 mkdir views ---> for html templates
 	--app.set("view engine", "ejs)
+
+
+mkdir views/partials ---> for css static
+	<%- include("partials/header") %>
+	--app.use
+	
+npm install body-parser --save
+	link to git
+	git init
+	git add *
+	git commit -m " asdasdasd "
+	git push -u origin master
